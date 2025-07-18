9b000cf18872fd675d447271341dc77563250eeb
calibr31 <www.yoyo123.94@gmail.com>
2025-07-18 03:43:31 -0700

    add log in secretenv2 updated functionality

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,11 +1,12 @@
 const jwt = require("jsonwebtoken");
-console.log("ACCESS_TOKEN_SECRET:", process.env.ACCESS_TOKEN_SECRET);
-console.log("REFRESH_TOKEN_SECRET:", process.env.REFRESH_TOKEN_SECRET);
+
 function generateAccessToken(payload) {
+  console.log("ACCESS_TOKEN_SECRET:", process.env.ACCESS_TOKEN_SECRET);
   const accesstoken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
   return accesstoken;
 }
 function generateRefreshToken(payload) {
+  console.log("REFRESH_TOKEN_SECRET:", process.env.REFRESH_TOKEN_SECRET);
   const refreshtoken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "5d" });
   return refreshtoken;
 }
